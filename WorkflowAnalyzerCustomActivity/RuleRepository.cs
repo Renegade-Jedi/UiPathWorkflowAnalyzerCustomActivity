1d7f56a1e88fcebb4238b40bdb71b9337a3da533
Renegade-jedi <tomekkac@gmail.com>
2020-07-08 17:11:06 +0200

    Quick Commit

--- WorkflowAnalyzerCustomActivity/RuleRepository.cs
+++ WorkflowAnalyzerCustomActivity/RuleRepository.cs
@@ -33,7 +33,44 @@ namespace WorkflowAnalyzerCustomActivity
 
         private InspectionResult InspectVariableForString(IActivityModel activityToInspect, Rule configuredRule)
         {
+            var configuredString = configuredRule.Parameters["string_in_variable"]?.Value;
 
+            if (string.IsNullOrEmpty(configuredString))
+            {
+                return new InspectionResult() { HasErrors = false };
+            }
+
+            if (activityToInspect.Variables.Count == 0)
+            {
+                return new InspectionResult() { HasErrors = false };
+            }
+
+            var messageList = new List<InspectionMessage>();
+
+            foreach (var variable in activityToInspect.Variables)
+            {
+                if (variable.DisplayName.Contains(configuredString))
+                {
+                    messageList.Add(new InspectionMessage()
+                    {
+                        Message = $"Variable {variable.DisplayName} contains an illegal string: {configuredString}"
+                    });
+                    
+                }
+            }
+
+            if(messageList.Count > 0)
+            {
+                return new InspectionResult()
+                {
+                    HasErrors = true,
+                    InspectionMessages = messageList,
+                    RecommendationMessage = "Fix your naming",
+                    ErrorLevel = configuredRule.ErrorLevel
+                };
+            }
+
+            return new InspectionResult() { HasErrors = false };
         }
     }
 }
